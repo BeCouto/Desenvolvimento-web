bae6f17d8c23c7e0d7f9a97828cbd814cd4a853e
Gabriel Couto <gcoutobarros@gmail.com>
2024-11-08 15:54:09 -0300

    .

--- DOM/code.js
+++ DOM/code.js
@@ -3,44 +3,70 @@ const url = "https://botafogo-atletas.mange.li/2024-1/";
 const pega_json = async (caminho) => {
     const resposta = await fetch(caminho);
     const dados = await resposta.json();
-    return dados
-}
+    return dados;
+};
+
 const container = document.getElementById("container");
 
-    let atleta;
+const manipulaClick = (e) => {
+    const id = e.currentTarget.dataset.id;
+    const url = `detalhes.html?id=${id}`;
 
- const montaCard = (atleta) => {
+    // Cookies com path e SameSite
+    document.cookie = `id=${id}; path=/; SameSite=Strict`;
+    document.cookie = `altura=${e.currentTarget.dataset.altura}; path=/; SameSite=Strict`;
+
+    // localStorage
+    localStorage.setItem('id', id);
+    localStorage.setItem('dados', JSON.stringify(e.currentTarget.dataset));
+    //sessionStorage
+    sessionStorage.setItem('dados', JSON.stringify(e.currentTarget.dataset))
+
+    window.location = url;
+};
+
+const montaCard = (atleta) => {
     const cartao = document.createElement("article");
     const nome = document.createElement("h1");
     const imagem = document.createElement("img");
     const descri = document.createElement("p");
-    const link = document.createElement("a");
 
     nome.innerHTML = atleta.nome;
-    nome.style.fontFamily = 'sains-serif'
+    nome.style.fontFamily = 'sans-serif';
     cartao.appendChild(nome);
 
     imagem.src = atleta.imagem;
-    cartao.appendChild(imagem)
+    cartao.appendChild(imagem);
 
     descri.innerHTML = atleta.detalhes;
     cartao.appendChild(descri);
 
-    link.innerText = 'saiba mais...'
-    link.href = `detalhes.html?id=${atleta.id}`;
-    cartao.appendChild(link);
+    cartao.onclick = manipulaClick; 
+
+    cartao.dataset.id = atleta.id;
+    cartao.dataset.nJogos = atleta.n_jogos;
+    cartao.dataset.altura = atleta.altura;
+
     return cartao;
+};
 
- };
+pega_json(`${url}masculino`).then((r) => {
+    r.forEach((ele) => container.appendChild(montaCard(ele)));
+});
 
-pega_json(`${url}masculino`).then(
-    (r) =>{
-        r.forEach(
-            (ele) => container.appendChild(montaCard(ele))
-        )
+pega_json(`${url}26`).then((r) => console.log(r));
+
+console.log("isso imprime primeiro.");
+
+const manipulaBotao = (e) => {
+    const texto = document.getElementById('senha').value;
+    if (hex_md5(texto) ==='5029cc9dd0295ded2f500084635c18c1'){
+        sessionStorage.setItem('logado', 'sim');
+    } else {
+        alert('Você errou a senha!!!');
     }
-);
+}
 
-pega_json(`${url}26`).then( (r) => console.log(r))
+document.getElementById('botao').onclick = manipulaBotao;
 
-console.log("isso imprime primeiro.")
+document.getElementById('logout').onclick = () => sessionStorage.removeItem('logout');
--- DOM/detalhes.js
+++ DOM/detalhes.js
@@ -20,9 +20,27 @@ const montaPagina = (dados) => {
     body.appendChild(imagem);
 }
 
+if(sessionStorage.getItem('logado')){
+
+
 pega_json(`https://botafogo-atletas.mange.li/2024-1/${id}`).then(
     ( r ) => montaPagina(r)
-);
+    );
+} else {
+    document.body.innerHTML = "<h1>Voce precisa estar logado</h1>"
+}
 
+const achaCookie = ( chave ) => {
+    const lista = document.cookie.split("; ");
+    const par = lista.find(
+        (e) => e.startsWith(`${chave}=`)
+    )
+    return par.split("=")[1]
+}
 
-console.log(params.get("id"));
+console.log('altura:' , achaCookie('altura'));
+
+const dadosSessionStorage = sessionStorage.getItem('dados');
+const obj = JSON.parse(dadosSessionStorage);
+
+console.log('numerodejogos:', obj.nJogos);
